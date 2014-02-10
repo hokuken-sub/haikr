bd7391d1812dccb77b638820302e02d3d862c980
masamori <r.masamori@gmail.com>
2014-02-10 16:40:07 +0900

    Create haik_users_table
    Create CreateHaikUsersTable class with migrate
    Create HaikUsersTableSeeder class and add a record by db:seed

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -2,17 +2,18 @@
 
 class DatabaseSeeder extends Seeder {
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Eloquent::unguard();
-
-		$this->call('HaikPagesTableSeeder');
-		$this->call('HaikSitesTableSeeder');
-	}
+    /**
+    * Run the database seeds.
+    *
+    * @return void
+    */
+    public function run()
+    {
+        Eloquent::unguard();
+        
+        $this->call('HaikPagesTableSeeder');
+        $this->call('HaikSitesTableSeeder');
+        $this->call('HaikUsersTableSeeder');
+    }
 
 }
