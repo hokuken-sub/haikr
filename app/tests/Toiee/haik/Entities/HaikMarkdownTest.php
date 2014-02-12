abb32e567e5cc1a85df00f9f4153404d671bb05d
Shun Hotta <stars.big2men@gmail.com>
2014-02-12 19:15:06 +0900

    Add HaikMarkdown::doHaikLinks.
    add below rules:
    - [[PageName]] -> Hypertext anchor tag to the page
    - [[Alias>PageName]] -> Hypertext anchor tag to the page with alias text
    - [[#hash]] -> Hypertext anchor tag to the hash within current page
    
    add test:
    - doHaikLinks
    - doInlinePlugins (prototype)

---
+++ app/tests/Toiee/haik/Entities/HaikMarkdownTest.php
@@ -0,0 +1,88 @@
+<?php
+use Toiee\haik\Entities\HaikMarkdown;
+
+class HaikMarkdownTest extends TestCase {
+    
+    public function testEmptyElementSuffix()
+    {
+        $parser = new HaikMarkdown;
+        $this->assertEquals('>', $parser->empty_element_suffix);
+    }
+    
+    public function testCodeClassPrefix()
+    {
+        $parser = new HaikMarkdown;
+        $this->assertEquals('', $parser->code_class_prefix);
+    }
+    
+    public function testDoHaikLinks()
+    {
+        $parser = new HaikMarkdown;
+
+        $tests = array(
+            'toppage' => array(
+                'markdown' => '[[FrontPage]]',
+                'assert'   => '<p><a href="http://localhost:8000" title="FrontPage">FrontPage</a></p>',
+            ),
+            'otherpage' => array(
+                'markdown' => '[[Contact]]',
+                'assert'   => '<p><a href="http://localhost:8000/Contact" title="Contact">Contact</a></p>',
+            ),
+            'toppage#hash' => array(
+                'markdown' => '[[FrontPage#hash]]',
+                'assert'   => '<p><a href="http://localhost:8000/#hash" title="FrontPage">FrontPage</a></p>',
+            ),
+            'otherpage#hash' => array(
+                'markdown' => '[[Contact#hash]]',
+                'assert'   => '<p><a href="http://localhost:8000/Contact#hash" title="Contact">Contact</a></p>',
+            ),
+            '>toppage' => array(
+                'markdown' => '[[Top>FrontPage]]',
+                'assert'   => '<p><a href="http://localhost:8000" title="Top">Top</a></p>',
+            ),
+            '>otherpage' => array(
+                'markdown' => '[[Touch me!>Contact]]',
+                'assert'   => '<p><a href="http://localhost:8000/Contact" title="Touch me!">Touch me!</a></p>',
+            ),
+            '#hashonly' => array(
+                'markdown' => '[[#hash]]',
+                'assert'   => '<p><a href="#hash">hash</a></p>',
+            ),
+            '>#hashonly' => array(
+                'markdown' => '[[Alias>#hash]]',
+                'assert'   => '<p><a href="#hash" title="Alias">Alias</a></p>',
+            ),
+        );
+        
+        foreach ($tests as $key => $data)
+        {
+            $this->assertEquals($data['assert'], trim($parser->transform($data['markdown'])));
+        }
+        
+    }
+    
+    public function testDoInlinePlugins()
+    {
+        $parser = new HaikMarkdown;
+        
+        $tests = array(
+            'normal_strong' => array(
+                'markdown' => '&deco(b){DECO};',
+                'assert'   => '<span class="haik-deco"><strong>DECO</strong></span>',
+            ),
+            'normal_strong_red' => array(
+                'markdown' => '&deco(b,red){DECO};',
+                'assert'   => '<span class="haik-deco" style="color:red"><strong>DECO</strong></span>',
+            ),
+            'with_markdown_strong' => array(
+                'markdown' => '&deco(b){*Italic*};',
+                'assert'   => '<span class="haik-deco"><strong><em>Italic</em></strong></span>',
+            ),
+        );
+        
+        foreach ($tests as $key => $data)
+        {
+            $this->assertEquals($data['assert'], $parser->transform($data['markdown']));
+        }
+    }
+}
