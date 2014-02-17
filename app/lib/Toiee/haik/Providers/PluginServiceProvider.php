306e0e1612d2f1e858fc8ca79c52322909781884
Shun Hotta <stars.big2men@gmail.com>
2014-02-17 12:18:24 +0900

    Fix PluginServiceProvider.
    move App::bind register to boot

--- app/lib/Toiee/haik/Providers/PluginServiceProvider.php
+++ app/lib/Toiee/haik/Providers/PluginServiceProvider.php
@@ -5,6 +5,14 @@ use Illuminate\Support\ServiceProvider;
 
 class PluginServiceProvider extends ServiceProvider {
     
+    public function boot()
+    {
+        $this->app->bind('PluginRepositoryInterface', function()
+        {
+            return new \Toiee\haik\Repositories\PluginRepository;
+        });
+    }
+    
     /**
      * register ServiceProvider
      */
@@ -13,10 +21,6 @@ class PluginServiceProvider extends ServiceProvider {
         $this->app['plugin'] = $this->app->share(function()
         {
             return new PluginManager();
-        });
-        $this->app->bind('PluginRepositoryInterface', function()
-        {
-            return new \Toiee\haik\Repositories\PluginRepository;
         });
     }
 }
