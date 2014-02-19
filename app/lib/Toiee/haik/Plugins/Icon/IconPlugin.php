7863a8682520fd96709933ff039275d66de617be
Keishi Tanaka <kc@toiee.jp>
2014-02-20 01:36:44 +0900

    Resolve icon plugin test of calling with params(ie. glyphicon, search)

--- app/lib/Toiee/haik/Plugins/Icon/IconPlugin.php
+++ app/lib/Toiee/haik/Plugins/Icon/IconPlugin.php
@@ -16,9 +16,28 @@ class IconPlugin extends Plugin {
     {
         if (count($params) === 0)
         {
-            // !TODO: paramsが無いとき、エラー処理する
+            // !TODO: threw error when no params
             return 'error';
         }
+
+
+        // 28: check 'glyphicon' exists in $params
+        // 30: check key of 'glyphicon'
+        // 31: alias to $icon_type because $params[key] will delete
+        // 33: delete 'glyphicon'
+        // 35: convert to string
+        if (in_array('glyphicon', $params))
+        {
+          $key = array_search('glyphicon', $params);
+          $icon_type = $params[$key];
+
+          unset($params[$key]);
+
+          $icon_value = implode('', $params);
+          return '<i class="'.$icon_type.' '.$icon_type.'-'.$icon_value.'">';
+
+        }
+
         return '';
     }
 }
