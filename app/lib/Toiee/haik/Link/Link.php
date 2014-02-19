72b0a3404c7fc1646bf2680c78a13fd58cc4f6ce
masamori <r.masamori@gmail.com>
2014-02-19 17:35:10 +0900

    Add Link resolver
    Add LinkResolverInterface
    Add LinkNotResolveException
    Modify Link class adding resolvers

--- app/lib/Toiee/haik/Link/Link.php
+++ app/lib/Toiee/haik/Link/Link.php
@@ -1,11 +1,29 @@
 <?php
 namespace Toiee\haik\Link;
 
+use Toiee\haik\Link\LinkResolverInterface;
+
 class Link implements LinkInterface {
     
+    protected $resolvers;
+    
+    public function __construct($resolvers=array())
+    {
+        $this->resolvers = $resolvers;
+    }
+        
     public function url($link)
     {
-        return $link;
+        try {
+            foreach ($this->resolvers as $resolver)
+            {
+                return $resolver->url($link);
+            }
+        }
+        catch(\Exception $e)
+        {
+            return $link;
+        }
     }
 
 }
---
+++ app/lib/Toiee/haik/Link/LinkResolverInterface.php
@@ -0,0 +1,14 @@
+<?php
+namespace Toiee\haik\Link;
+
+interface LinkResolverInterface {
+    
+    /**
+     * return url
+     * @params string $link page name or url or ...
+     * @return string converted url
+     * @throws LinkNotResolveException when link is not found
+     */
+    public function url($link);
+
+}
