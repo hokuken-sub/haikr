f1082d840e3793fe4fc2ea42f91fd1f38832c295
masamori <r.masamori@gmail.com>
2014-02-19 18:05:26 +0900

    Create PageResolver

---
+++ app/lib/Toiee/haik/Link/PageResolver.php
@@ -0,0 +1,22 @@
+<?php
+namespace Toiee\haik\Link;
+
+class PageResolver implements LinkResolverInterface
+{
+
+    /**
+     * return url
+     * @params string $link page name or url or ...
+     * @return string converted url
+     * @throws LinkNotResolveException when page is not found
+     */
+    public function url($link)
+    {
+        if (Haik::pageExists($link))
+        {
+            return Haik::url($link);
+        }
+        
+        throw new LinkNotResolveException;    
+    }
+}
