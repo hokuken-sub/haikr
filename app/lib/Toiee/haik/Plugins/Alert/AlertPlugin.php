2c6c8fd9400274c28cd39fa02de6ae64c3ce1bf1
Keishi Tanaka <kc@toiee.jp>
2014-02-20 15:44:23 +0900

    Create AlertPlugin.php with convert()

---
+++ app/lib/Toiee/haik/Plugins/Alert/AlertPlugin.php
@@ -0,0 +1,12 @@
+<?php
+namespace Toiee\haik\Plugins\Alert;
+
+use Toiee\haik\Plugins\Plugin;
+
+class AlertPlugin extends Plugin {
+
+    public function convert($params = array(), $body = '')
+    {
+        return '';
+    }
+}
