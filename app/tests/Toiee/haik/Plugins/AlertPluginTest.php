9ce96eb4c201b5921fc6b38b94a92f1270b9292a
Keishi Tanaka <kc@toiee.jp>
2014-02-20 16:30:18 +0900

    Add test call alert plugin with no params ( unsoleved )

--- app/tests/Toiee/haik/Plugins/AlertPluginTest.php
+++ app/tests/Toiee/haik/Plugins/AlertPluginTest.php
@@ -6,7 +6,25 @@ class AlertPluginTest extends TestCase {
     public function testConvertMethodExists()
     {
         $this->assertInternalType('string', with(new AlertPlugin)->convert());
+    }
 
+    public function testParameter()
+    {
+        $tests = array(
+            'no_params' => array(
+                'alert' => array(),
+                'assert' => '<div class="alert alert-warning">test</div>',
+            ),
+        );
+
+        foreach ($tests as $key => $data)
+        {
+            $this->assertEquals($data['assert'], with(new AlertPlugin)->convert($data['alert']));
+        }
+
+        $this->markTestIncomplete(
+            'This test is Incomplete.'
+        );
     }
 
 }
