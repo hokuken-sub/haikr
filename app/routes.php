8c5559a55267589ea8d7ab006b1c600618d9b580
Shun Hotta <stars.big2men@gmail.com>
2014-02-20 15:33:12 +0900

    Change admin routing

--- app/routes.php
+++ app/routes.php
@@ -13,27 +13,27 @@
 
 Route::get('/', 'PageController@show');
 
-Route::when('haik-admin*', 'auth');
+Route::when('haik--admin*', 'auth');
 
-// URL: /haik-admin/create/
-Route::get('/haik-admin/create', 'PageController@create');
-Route::post('/haik-admin/create', 'PageController@create');
+// URL: /haik--admin/create/
+Route::get('/haik--admin/create', 'PageController@create');
+Route::post('/haik--admin/create', 'PageController@create');
 
 
-// URL: /haik-admin/edit/{pagename}
+// URL: /haik--admin/edit/{pagename}
 // で edit: {pagename} を表示する
-Route::get('/haik-admin/edit/{pagename}', 'PageController@edit');
-Route::post('/haik-admin/edit', 'PageController@store');
+Route::get('/haik--admin/edit/{pagename}', 'PageController@edit');
+Route::post('/haik--admin/edit', 'PageController@store');
 
 
-// URL: /haik-admin/destroy/{pagename}
+// URL: /haik--admin/destroy/{pagename}
 // で {pagename}を削除する
 
-Route::get('/haik-admin/destroy/{pagename}', 'PageController@destroy');
+Route::get('/haik--admin/destroy/{pagename}', 'PageController@destroy');
 
 
-Route::get('haik-admin/site/settings', 'SiteController@settings');
-Route::post('haik-admin/site/settings', 'SiteController@store');
+Route::get('haik--admin/site/settings', 'SiteController@settings');
+Route::post('haik--admin/site/settings', 'SiteController@store');
 
 Route::any('/login', array('uses' => 'SessionController@login', 'as' => '/login'));
 Route::get('/logout', array('uses' => 'SessionController@logout', 'as' => '/logout'));
