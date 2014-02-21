367a16fd6918d129170983be9ea683495ab81be7
Keishi Tanaka <kc@toiee.jp>
2014-02-21 16:06:27 +0900

    Create PanePlugin.php with convert()

---
+++ app/lib/Toiee/haik/Plugins/Panel/PanelPlugin.php
@@ -0,0 +1,19 @@
+<?php
+namespace Toiee\haik\Plugins\Panel;
+
+use Toiee\haik\Plugins\Plugin;
+
+class PanelPlugin extends Plugin {
+  
+    /**
+     * convert call via HaikMarkdown {#plugin-name}
+     * @params array $params
+     * @params string $body when :::\n...\n::: was set
+     * @return string converted HTML string
+     * @throws RuntimeException when unimplement
+     */
+    public function convert($params = array(), $body = '')
+    {
+      
+    }
+}
