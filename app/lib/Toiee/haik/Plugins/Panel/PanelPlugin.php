76d85e334eb27ec5a7eee3e583a8b774e021b0b8
Keishi Tanaka <kc@toiee.jp>
2014-02-22 18:10:03 +0900

    Resolve PanelPluginTest::testHeadingParse
    Use preg_match() & preg_replace to flexible parsing.

--- app/lib/Toiee/haik/Plugins/Panel/PanelPlugin.php
+++ app/lib/Toiee/haik/Plugins/Panel/PanelPlugin.php
@@ -33,10 +33,29 @@ class PanelPlugin extends Plugin {
             }
         }
 
+        # check $body has "====".
         if (preg_match("/\n====\n/", $body))
         {
             $body = explode("\n====\n", $body);
             list($title, $content) = $body;
+
+            # check $body after parsed is heading.
+            if (preg_match("/(<h[1-6].*?>).*?(<\/h[1-6]>)/", \Parser::parse($title)))
+            {
+                # start check detail
+                switch (true)
+                {
+                    # if user use markdown, put {.panel-title}
+                    case preg_match_all("/^#{1,6}/", $title):
+                        $title = $title.' {.panel-title}';
+                        break;
+
+                    # if user use raw html without panel-title class, put it.
+                    case preg_match("/<h[1-6]>/", $title):
+                        $title = preg_replace("/>/", " class=\"panel-title\">", $title, 1);
+                        break;
+                }
+            }
 
             return '<div class="haik-plugin-panel '.$base_class.' '.$prefix.$type.'">'
                  . '<div class="panel-heading">'.\Parser::parse($title).'</div>'
