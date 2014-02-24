21a23374c77e36e0754dd1f8ab58d3882f6e9ebd
masamori <r.masamori@gmail.com>
2014-02-24 11:44:34 +0900

    Add cols plugin
    Resoleve test that exists convert method

---
+++ app/lib/Toiee/haik/Plugins/Cols/ColsPlugin.php
@@ -0,0 +1,15 @@
+<?php
+namespace Toiee\haik\Plugins\Cols;
+
+use Toiee\haik\Plugins\Plugin;
+
+class ColsPlugin extends Plugin {
+
+    public function convert($params = array(), $body = '')
+    {
+        $html = '';
+
+        return $html;
+    }
+
+}
