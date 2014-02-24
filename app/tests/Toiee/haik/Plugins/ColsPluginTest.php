83e347f0221832440d8f3d2c3dea9058ccb7179e
masamori <r.masamori@gmail.com>
2014-02-24 12:44:43 +0900

    Add params test

--- app/tests/Toiee/haik/Plugins/ColsPluginTest.php
+++ app/tests/Toiee/haik/Plugins/ColsPluginTest.php
@@ -9,4 +9,29 @@ class ColsPluginTest extends TestCase {
         $this->assertInternalType('string', with(new ColsPlugin)->convert());
     }
 
+    public function testParameter()
+    {
+        $tests = array(
+            'no_params' => array(
+                'cols' => array(),
+                'assert' => array(
+                    0 => array (
+                        'cols'   => 12,
+                        'offset' => 0,
+                        'class'  => '',
+                        'style'  => '',
+                        'body'   => '',
+                    ),
+                )
+            ),
+        );
+
+        foreach ($tests as $key => $data)
+        {
+            $cols = new ColsPlugin;
+            $cols->convert($data['cols'], '');
+            $this->assertAttributeSame($data['assert'], 'cols', $cols);
+        }
+    }
+
 }
