81c359bbe189c133b542920bd1462badbb7f10f3
Shun Hotta <stars.big2men@gmail.com>
2014-02-25 08:17:23 +0900

    Add tests

--- app/tests/Toiee/haik/Themes/ThemeConfigParserTest.php
+++ app/tests/Toiee/haik/Themes/ThemeConfigParserTest.php
@@ -1,9 +1,15 @@
 <?php
 use Toiee\haik\Themes\ThemeConfigParser;
+use Config;
 
 class ThemeConfigParserTest extends TestCase {
 
-    public function testParseFullStackOptions()
+    public function setUp()
+    {
+        $this->parser = new ThemeConfigParser;
+    }
+
+    public function testFullStackOptions()
     {
         $full_stack_options = array(
             'name' => 'kawaz',
@@ -30,9 +36,98 @@ class ThemeConfigParserTest extends TestCase {
             )
         );
         
-        $parser = App::make('ThemeConfigParserInterface');
-        $result = $parser->parse($full_stack_options);
+        $result = $this->parser->parse($full_stack_options);
         
         $this->assertEquals($full_stack_options, $result);
+    }
+    
+    public function testShorthandLayoutsOptions()
+    {
+        $shorthand_options = array(
+            'name' => 'kawaz',
+            'version' => '1.0.0',
+            'layouts' => array("top", "content"),
+            'default_layout' => 'top',
+        );
+        $default_partials = Config::get('theme.partials.default', array());
+        $expected_layouts = array(
+            'top' => array(
+                'filename' => 'top.layout.blade.php',
+                'partials' => $default_partials,
+                'thumbnail' => 'assets/images/top.thumbnail.png',
+            ),
+            'content' => array(
+                'filename' => 'content.layout.blade.php',
+                'partials' => $default_partials,
+                'thumbnail' => 'assets/images/content.thumbnail.png',
+            ),
+        );
+        
+        $result = $this->parser->parse($shorthand_options);
+        
+        $this->assertEquals($expected_layouts, $result['layouts']);
+    }
+    
+    public function testShorthandColorsOptions()
+    {
+        $shorthand_options = array(
+            'name' => 'kawaz',
+            'version' => '1.0.0',
+            'layouts' => array("top", "content"),
+            'default_layout' => 'top',
+            'colors' => array('black', 'white', 'raspberry'),
+        );
+        $expected_colors = array(
+            'black' => array(
+                'className' => 'haik-theme-kawaz-color-black',
+            ),
+            'white' => array(
+                'className' => 'haik-theme-kawaz-color-white',
+            ),
+            'raspberry' => array(
+                'className' => 'haik-theme-kawaz-color-raspberry',
+            ),
+        );
+
+        $result = $this->parser->parse($shorthand_options);
+        $this->assertEquals($expected_colors, $result['colors']);
+    }
+    
+    public function testShorthandTexturesOptions()
+    {
+        $shorthand_options = array(
+            'name' => 'kawaz',
+            'version' => '1.0.0',
+            'layouts' => array("top", "content"),
+            'default_layout' => 'top',
+            'textures' => array('hemp', 'tile', 'stripe'),
+        );
+        $expected_textures = array(
+            'hemp' => array(
+                'className' => 'haik-theme-kawaz-texture-hemp',
+            ),
+            'tile' => array(
+                'className' => 'haik-theme-kawaz-texture-tile',
+            ),
+            'stripe' => array(
+                'className' => 'haik-theme-kawaz-texture-stripe',
+            ),
+        );
+
+        $result = $this->parser->parse($shorthand_options);
+        $this->assertEquals($expected_textures, $result['textures']);
+    }
+    
+    public function testDefaultLayoutNotSet()
+    {
+        $shorthand_options = array(
+            'name' => 'kawaz',
+            'version' => '1.0.0',
+            'layouts' => array('top', 'content'),
+        );
+        $expected = 'top';
+        
+        $result = $this->parser->parse($shorthand_options);
+        $this->assertEquals($expected, $result['default_layout']);
     }
 }
