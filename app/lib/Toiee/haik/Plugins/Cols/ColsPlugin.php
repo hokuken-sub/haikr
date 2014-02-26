ad6e334346f1052e2a06647db4b3a2b13b2b77b0
masamori <r.masamori@gmail.com>
2014-02-26 17:42:23 +0900

    Fix cols plugin for thumbnail plugin

--- app/lib/Toiee/haik/Plugins/Cols/ColsPlugin.php
+++ app/lib/Toiee/haik/Plugins/Cols/ColsPlugin.php
@@ -5,8 +5,15 @@ use Toiee\haik\Plugins\Plugin;
 
 class ColsPlugin extends Plugin {
 
-    const COL_MAX_NUM  = 12;
-    const COL_DELIMITER = "\n====\n";
+    const COL_MAX_NUM     = 12;
+    const COL_DELIMITER   = "\n====\n";
+    const COL_FORMAT_EACH = '  <div class="%s" style="%s">%s</div>';
+
+    const ROW_FORMAT      = <<< EOD
+<div class="haik-plugin-cols row %s">
+%s
+</div>
+EOD;
 
     protected $className;
     protected $delimiter;
@@ -144,25 +151,34 @@ class ColsPlugin extends Plugin {
     protected function getHtml()
     {
         $col_body = array();
-        $col_format = '<div class="%s" style="%s">%s</div>';
-        $top_class = $this->className ? (' ' . $this->className) : '';
+        $top_class = $this->className ? $this->className : '';
 
         foreach ($this->cols as $col)
         {
             $span   = 'col-sm-'.$col['cols'];
             $offset = $col['offset'] ? (' col-sm-offset-' . $col['offset']) : '';
             $class  = $col['class']  ? (' ' . $col['class']) : '';
-            $style  = $col['style']  ? $col['style'] : '';
-            
-            $body = \Parser::parse($col['body']);
 
-            $col_body[] = sprintf($col_format, ($span . $offset . $class), $style, $body);
+            $coldata = array();
+            $coldata[] = $span . $offset . $class;
+            $coldata[] = $col['style']  ? $col['style'] : '';
+            $coldata[] = $col['body'];
+
+            $col_body[] = $this->setFormatEach($coldata);
         }
         
-        $html = '<div class="haik-plugin-cols row'.$top_class.'">'."\n".join("\n", $col_body)."\n".'</div>';
+        $c = get_called_class();
+        $html = sprintf($c::ROW_FORMAT, $top_class, join("\n", $col_body));
         return $html;
     }
     
+    protected function setFormatEach($data)
+    {
+        $c = get_called_class();
+        $body = \Parser::parse($data[2]);
+        $html = sprintf($c::COL_FORMAT_EACH, $data[0], $data[1], $body);
+        return $html;
+    }
     
     /**
      * parse cols param
@@ -172,6 +188,7 @@ class ColsPlugin extends Plugin {
     protected function parseParam($param)
     {
         $data = array();
+        
         if (preg_match('/^(\d+)(?:\+(\d+))?((?:\.[a-zA-Z0-9_-]+)+)?$/', $param, $mts))
         {
             $data['cols']   = (int)$mts[1];
