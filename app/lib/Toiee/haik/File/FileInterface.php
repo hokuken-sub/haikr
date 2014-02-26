8313506ec445760e1c7e08dbb1721d66c3457ca5
Shun Hotta <stars.big2men@gmail.com>
2014-02-26 17:43:29 +0900

    Reduce methods of  FileInterface

--- app/lib/Toiee/haik/File/FileInterface.php
+++ app/lib/Toiee/haik/File/FileInterface.php
@@ -4,21 +4,6 @@ namespace Toiee\haik\File;
 interface FileInterface {
 
     /**
-     * Save the file as new file
-     */
-    public function save();
-
-    /**
-     * Update the file
-     */
-    public function update();
-
-    /**
-     * Delete the file
-     */
-    public function delete();
-
-    /**
      * Mark star the file
      */
     public function star();
