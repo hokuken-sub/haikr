e2adc4be4b48773a67bb548563c0e18b863d8fe1
Keishi Tanaka <kc@toiee.jp>
2014-02-26 13:46:22 +0900

    Add UtilityTest::testParameter with data provider

--- app/tests/Toiee/haik/Plugins/UtilityTest.php
+++ app/tests/Toiee/haik/Plugins/UtilityTest.php
@@ -14,4 +14,63 @@ class UtilityTest extends TestCase {
         # return FALSE if parseColumnData not work correctly.
         $this->assertFalse(Utility::parseColumnData('three'));
     }
+
+    /**
+     * @dataProvider columnDataProvider
+     */
+    public function testParameter($columndata, $assert)
+    {
+        $this->assertEquals($assert, Utility::parseColumnData($columndata));
+    }
+
+    public function columnDataProvider()
+    {
+        $tests = array(
+            'no_data' => array(
+                'columndata' => '',
+                'assert'     => false,
+            ),
+            'one_num_data' => array(
+                'columndata' => '12',
+                'assert'     => array(
+                    'cols'   => 12,
+                    'offset' => 0,
+                    'class'  => '',
+                ),
+            ),
+            'num_and_offset_data' => array(
+                'columndata' => '3+3',
+                'assert'     => array(
+                    'cols'   => 3,
+                    'offset' => 3,
+                    'class'  => '',
+                ),
+            ),
+            'num_and_class_data' => array(
+                'columndata' => '3.classname',
+                'assert'     => array(
+                    'cols'   => 3,
+                    'offset' => 0,
+                    'class'  => 'classname',
+                ),
+            ),
+            'num_and_doubleclass_data' => array(
+                'columndata' => '3.classname1.classname2',
+                'assert'     => array(
+                    'cols'   => 3,
+                    'offset' => 0,
+                    'class'  => 'classname1 classname2',
+                ),
+            ),
+            'num_and_offset_and_class_data' => array(
+                'columndata' => '3+3.classname',
+                'assert'     => array(
+                    'cols'   => 3,
+                    'offset' => 3,
+                    'class'  => 'classname',
+                ),
+            ),
+        );
+        return $tests;
+    }
 }
