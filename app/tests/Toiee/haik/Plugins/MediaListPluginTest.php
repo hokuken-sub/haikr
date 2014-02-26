f5e5b09c6dcf52fe45b01a19d80cf518d5e349ca
Keishi Tanaka <kc@toiee.jp>
2014-02-26 17:02:16 +0900

    Add tests to MediaListPlugin to check working utility correctly

--- app/tests/Toiee/haik/Plugins/MediaListPluginTest.php
+++ app/tests/Toiee/haik/Plugins/MediaListPluginTest.php
@@ -180,4 +180,135 @@ class MediaListPluginTest extends TestCase {
 
         $this->assertEquals($test['assert'], with(new MediaListPlugin)->convert($test['medialist'], $body));
     }
+
+    public function testMediaListWithColumn()
+    {
+        $main = '<div class="media">'
+                . '<span class="pull-left">'
+                . '<img class="media-object" src="http://placehold.jp/80x80.png" alt="alt">'
+                . '</span>'
+                . '<div class="media-body">'
+                . '<h4 class="media-heading">test title</h4>'
+                . '<p>test</p>'
+                . '</div></div>';
+
+        # This is the test of medialists with param only cols.
+        $start_tag = '<div class="row"><div class="col-sm-6">';
+        $close_tag = '</div></div>';
+        $test = array(
+            'medialist' => array('6'),
+            'assert' => $start_tag.$main.$close_tag,
+        );
+
+        $body = "![alt](http://placehold.jp/80x80.png)\n"
+              . "#### test title\n"
+              . "test\n";
+
+        $this->assertEquals($test['assert'], with(new MediaListPlugin)->convert($test['medialist'], $body));
+
+
+        # This is the test of medialists with param cols & offset.
+        $start_tag = '<div class="row"><div class="col-sm-5 col-sm-offset-1">';
+        $close_tag = '</div></div>';
+        $test = array(
+            'medialist' => array('5+1'),
+            'assert' => $start_tag.$main.$close_tag,
+        );
+
+        $body = "![alt](http://placehold.jp/80x80.png)\n"
+              . "#### test title\n"
+              . "test\n";
+
+        $this->assertEquals($test['assert'], with(new MediaListPlugin)->convert($test['medialist'], $body));
+
+
+        # This is the test of medialists with param cols & class.
+        $start_tag = '<div class="row"><div class="col-sm-5 col-sm-offset-1">';
+        $close_tag = '</div></div>';
+        $test = array(
+            'medialist' => array('6.well'),
+            'assert' => $start_tag.$main.$close_tag,
+        );
+
+        $body = "![alt](http://placehold.jp/80x80.png)\n"
+              . "#### test title\n"
+              . "test\n";
+
+        $this->assertEquals($test['assert'], with(new MediaListPlugin)->convert($test['medialist'], $body));
+
+
+        # This is the test of medialists with param cols & offset & class.
+        $start_tag = '<div class="row"><div class="col-sm-5 col-sm-offset-1 well">';
+        $close_tag = '</div></div>';
+        $test = array(
+            'medialist' => array('5+1.well'),
+            'assert' => $start_tag.$main.$close_tag,
+        );
+
+        $body = "![alt](http://placehold.jp/80x80.png)\n"
+              . "#### test title\n"
+              . "test\n";
+
+        $this->assertEquals($test['assert'], with(new MediaListPlugin)->convert($test['medialist'], $body));
+
+
+        # This is the test of medialists with param cols & offset & double class.
+        $start_tag = '<div class="row"><div class="col-sm-5 col-sm-offset-1 well text-success">';
+        $close_tag = '</div></div>';
+        $test = array(
+            'medialist' => array('5+1.panel.panel-default'),
+            'assert' => $start_tag.$main.$close_tag,
+        );
+
+        $body = "![alt](http://placehold.jp/80x80.png)\n"
+              . "#### test title\n"
+              . "test\n";
+
+        $this->assertEquals($test['assert'], with(new MediaListPlugin)->convert($test['medialist'], $body));
+
+
+        # This is the test of multi medialists with param cols & offset & double class.
+        $start_tag = '<div class="row"><div class="col-sm-5 col-sm-offset-1 well text-success">';
+        $close_tag = '</div></div>';
+        $test = array(
+            'medialist' => array('5+1.panel.panel-default'),
+            'assert' => $start_tag.$main.$main.$close_tag,
+        );
+
+        $body = "![alt](http://placehold.jp/80x80.png)\n"
+              . "#### test title\n"
+              . "test\n"
+              . "====\n"
+              . "![alt](http://placehold.jp/80x80.png)\n"
+              . "#### test title\n"
+              . "test\n";
+
+        $this->assertEquals($test['assert'], with(new MediaListPlugin)->convert($test['medialist'], $body));
+    }
+
+    public function testEscapeParameter()
+    {
+        $main = '<div class="media">'
+                . '<span class="pull-left">'
+                . '<img class="media-object" src="http://placehold.jp/80x80.png" alt="alt">'
+                . '</span>'
+                . '<div class="media-body">'
+                . '<h4 class="media-heading">test title</h4>'
+                . '<p>test</p>'
+                . '</div></div>';
+
+        # This is the test of medialists with param cols & offset & double class.
+        $start_tag = '<div class="row"><div class="col-sm-6 &lt;hogehoge&gt;">';
+        $close_tag = '</div></div>';
+        $test = array(
+            'medialist' => array('6.<hogehoge>'),
+            'assert' => $start_tag.$main.$close_tag,
+        );
+
+        $body = "![alt](http://placehold.jp/80x80.png)\n"
+              . "#### test title\n"
+              . "test\n";
+
+        $this->assertEquals($test['assert'], with(new MediaListPlugin)->convert($test['medialist'], $body));
+    }
 }
