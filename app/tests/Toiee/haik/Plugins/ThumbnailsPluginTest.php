eca4ba6eba27d28f159e22b72a930d6252b56414
masamori <r.masamori@gmail.com>
2014-02-26 18:33:05 +0900

    Fix thumbnails plugin test

--- app/tests/Toiee/haik/Plugins/ThumbnailsPluginTest.php
+++ app/tests/Toiee/haik/Plugins/ThumbnailsPluginTest.php
@@ -73,46 +73,6 @@ class ThumbnailsPluginTest extends TestCase {
                             '  </div>'."\n".
                             '</div>',
             ),
-/*
-            'nodelimiter' => array(
-                'cols' => array('3+2','4+1.starbucks','class=tea'),
-                'body' => "STYLE:background-color:#000;color:#ccc;\n".
-                          "CLASS:burbon\n".
-                          "col1\n".
-                          "\n====\n".
-                          "STYLE:background-color:#f33;color:#222;\n".
-                          "CLASS:cafe\n".
-                          "col2\n".
-                          "col3",
-                'assert' => '<div class="haik-plugin-thumbnails row tea">'."\n".
-                            '<div class="col-sm-3 col-sm-offset-2 burbon" style="background-color:#000;color:#ccc;">'.\Parser::parse("col1").'</div>'."\n".
-                            '<div class="col-sm-4 col-sm-offset-1 starbucks cafe" style="background-color:#f33;color:#222;">'.\Parser::parse("col2\ncol3").'</div>'."\n".
-                            '</div>',
-            ),
-            'diffColsOverBody' => array(
-                'cols' => array(6,6),
-                'body' => "col1\n".
-                          "\n====\n".
-                          "col2\n".
-                          "\n====\n".
-                          "col3",
-                'assert' => '<div class="haik-plugin-thumbnails row">'."\n".
-                            '<div class="col-sm-6" style="">'.\Parser::parse("col1").'</div>'."\n".
-                            '<div class="col-sm-6" style="">'.\Parser::parse("col2\n\n====\ncol3").'</div>'."\n".
-                            '</div>',
-            ),
-            'diffColsLessBody' => array(
-                'cols' => array(4,4,4),
-                'body' => "col1\n".
-                          "\n====\n".
-                          "col2",
-                'assert' => '<div class="haik-plugin-thumbnails row">'."\n".
-                            '<div class="col-sm-4" style="">'.\Parser::parse("col1").'</div>'."\n".
-                            '<div class="col-sm-4" style="">'.\Parser::parse("col2").'</div>'."\n".
-                            '<div class="col-sm-4" style="">'."\n".'</div>'."\n".
-                            '</div>',
-            ),
-*/
         );
 
         foreach ($tests as $key => $data)
