a2e462e2c27b80239b3b36f760c68e0ae29a207c
Shun Hotta <stars.big2men@gmail.com>
2014-02-27 17:55:54 +0900

    Add test for FileManager

---
+++ app/tests/Toiee/haik/File/FileManagerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Toiee\haik\File\FileManager;
+
+class FileManagerTest extends TestCase {
+
+    public function testFileGetContent()
+    {
+        
+    }
+
+    public function testFileSaveContent()
+    {
+        
+    }
+
+    public function testFileDelete()
+    {
+        
+    }
+
+    public function testAccess()
+    {
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    public function testDownload()
+    {
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+}
