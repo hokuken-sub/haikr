3216ead3cc685d6b31df57a0ee9f1bfaed670832
Shun Hotta <stars.big2men@gmail.com>
2014-02-27 15:46:48 +0900

    Add FileManager

---
+++ app/lib/Toiee/haik/File/FileManager.php
@@ -0,0 +1,213 @@
+<?php
+namespace Toiee\haik\File;
+
+class FileManager {
+
+    const IDENTIFIER_REGEX = '/\A[0-9a-zA-Z_.-]+\z/';
+    const AUTO_IDENTIFIER_SEED = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const AUTO_IDENTIFIER_MIN_LENGTH = 4;
+    const AUTO_IDENTIFIER_TRY_LIMIT = 10000;
+
+    /**
+     * FileRepositoryInterface
+     */
+    protected $files;
+
+    /**
+     * Available storage drivers
+     */
+    protected $storageDrivers = array();
+
+
+    public function __construct(FileRepositoryInterface $repository)
+    {
+        $this->files = $repository;
+    }
+
+    public function listGet($page = 1)
+    {
+        return $this->files->listGet($page);
+    }
+
+    public function listByType($type, $page = 1)
+    {
+        return $this->files->listByType($type, $page);
+    }
+
+    public function listStarred($page = 1)
+    {
+        return $this->files->listStarred($page);
+    }
+
+    public function fileExists($identifier)
+    {
+        return $this->files->exists($identifier);
+    }
+
+    public function fileGet($identifier)
+    {
+        return $this->files->retrieve($identifier);
+    }
+
+    /**
+     * Get file content.
+     * When file not found then system delete the file record and return false.
+     *
+     * @param string $identifier
+     * @return string|false file content or false
+     */
+    public function fileGetContent($identifier)
+    {
+        $file = $this->fileGet($identifier);
+
+        try {
+            return $this->getStorageDriver($file->storage)->get($file->getIdentifier());
+        }
+        catch (Exception $c)
+        {
+            $file->delete();
+            return false;
+        }
+    }
+
+    /**
+     * Save file content.
+     *
+     * @param FileInterface $file Prepared FileInterface object
+     * @param string $content
+     * @return boolean when success return true
+     */
+    public function fileSaveContent($file, $content = '')
+    {
+        if ($file->exists)
+        {
+            $identifier = $file->getIdentifier();
+            $file = $this->fileGet($identifier);
+            if ($file->getStorageDriver($file->storage)->save($identifier, $content))
+            {
+                $file->touch();
+                return true;
+            }
+        }
+        else
+        {
+            // When file is set identifier, use it.
+            $identifier = trim($this->getIdentifier());
+            if ($identifier === '' OR $this->files->exists($identifier))
+            {
+                $identifier = $this->createIdentifier();
+            }
+
+            if ($this->getStorageDriver()->save($identifier, $content))
+            {
+                $file->save();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function fileDelete($identifier)
+    {
+        if ($this->fileExists($identifier))
+        {
+            $file = $this->fileGet($identifier);
+            $file->getStorageDriver($file->storage)->delete($identifier);
+        }
+        return false;
+    }
+    public function access($identifier)
+    {
+        $file = $this->fileGet($identifier);
+        $this->getStorageDriver($file->storage)->passthru($file->getIdentifier());
+        exit;
+    }
+
+    public function download($identifier)
+    {
+        $file = $this->fileGet($identifier);
+        $this->getStorageDriver($file->storage)->download($file->getIdentifier());
+    }
+
+    protected function createIdentifier(FileInterface $file = null)
+    {
+        do
+        {
+            $identifier = $this->generateIdentifier($file);
+        }
+        while ($this->files->exists($identifier));
+        return $identifier;
+    }
+    
+    protected function generateIdentifier(FileInterface $file = null)
+    {
+        $identifier = '';
+        $seed_length = strlen(AUTO_IDENTIFIER_SEED);
+        $length = $this->getAutoIdentifierLength();
+        for ($i = 0; $i < $length; $i++)
+        {
+            $index = rand(0, $seed_length - 1);
+            $identifier .= substr(AUTO_IDENTIFIER_SEED, $index, 1);
+        }
+        return $identifier;
+    }
+    
+    protected function getAutoIdentifierLength()
+    {
+        static $call_count = 0,
+               $increment = 0;
+        $call_count++;
+        if ($call_count > AUTO_IDENTIFIER_TRY_LIMIT)
+        {
+            $increment++;
+            $call_count = 0;
+        }
+        return AUTO_IDENTIFIER_MIN_LEN + $increment;
+    }
+
+    protected function validateIdentifier($identifier)
+    {
+        return preg_match(IDENTIFIER_REGEX, $identifier);
+    }
+
+    protected function getStorageDriver($storage)
+    {
+        if (array_key_exists($storage, $this->storageDrivers))
+        {
+            return $this->storageDrivers[$storage];
+        }
+        return $this->createStorageDriver($storage);
+    }
+
+    protected function storageDrivers()
+    {
+        return $this->storageDrivers;
+    }
+
+    protected function createStorageDriver($storage)
+    {
+        $method_name = camel_case('create_' . $storage . '_storage_driver');
+        if (method_exists($this, $method_name))
+        {
+            return $this->$method_name();
+        }
+        return null;
+    }
+
+    protected function createLocalStorageDriver()
+    {
+        return $this->storageDrivers['local'] = new LocalStorage();
+    }
+
+    protected function createDatabaseStorageDriver()
+    {
+        // TODO: create DatabaseStorage
+        return null;
+    }
+
+    protected function createS3StorageDriver()
+    {
+        // !TODO: create S3Storage
+        return null;
+    }
+}
