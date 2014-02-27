587862e5bac239f0c430cd19b65a807406395eb7
Shun Hotta <stars.big2men@gmail.com>
2014-02-27 19:20:20 +0900

    Add FileServiceProvider

--- app/lib/Toiee/haik/File/FileServiceProvider.php
+++ app/lib/Toiee/haik/File/FileServiceProvider.php
@@ -7,17 +7,38 @@ class FileServiceProvider extends ServiceProvider {
 
     public function boot()
     {
-        $this->app->bind('LocalStorage', function()
-        {
-            return new LocalStorage;
-        });
-        $this->app->singleton('FileManager', function($app)
-        {
-            return new FileManager;
-        });
+        $this->registerLocalStorage();
+        $this->registerFileRepositoryInterface();
+        $this->registerFileManager();
     }
 
     public function register()
     {
+    }
+
+    public function registerLocalStorage()
+    {
+        $this->app->bind('LocalStorage', function()
+        {
+            return new LocalStorage;
+        });
+    }
+
+    public function registerFileRepositoryInterface()
+    {
+        $this->app->bind('FileRepositoryInterface', function()
+        {
+            return new FileRepository;
+        });
+    }
+
+    public function registerFileManager()
+    {
+        $this->app->singleton('FileManager', function($app)
+        {
+            return new FileManager(
+                $app->make('FileRepositoryInterface')
+            );
+        });
     }
 }
