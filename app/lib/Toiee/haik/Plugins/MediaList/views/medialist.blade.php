efc96400cd64bf048109ff1dba8445be4cdfb122
Keishi Tanaka <kc@toiee.jp>
2014-03-10 12:40:40 +0900

    Fix if statement in medialist.blade.php to show markup correctly

--- app/lib/Toiee/haik/Plugins/MediaList/views/medialist.blade.php
+++ app/lib/Toiee/haik/Plugins/MediaList/views/medialist.blade.php
@@ -4,7 +4,7 @@
     <span class="{{ $item['align'] or "pull-left" }}">
         {{ $item['image'] or $defaultImage }}
     </span>
-    @if (isset($item['heading']) && isset($item['body']))
+    @if ((isset($item['heading']) && $item['heading'] !== '') or $item['body'] !== '')
     <div class="media-body">
         {{ $item['heading'] or '' }}
         {{ $item['body'] }}
