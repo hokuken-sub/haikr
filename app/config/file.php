80bb68b45d3afca44a0a708bae4772822873232d
Shun Hotta <stars.big2men@gmail.com>
2014-03-10 17:32:57 +0900

    Add comments and value.
    Set default input:file name attributes value

--- app/config/file.php
+++ app/config/file.php
@@ -1,10 +1,30 @@
 <?php
 
 return array(
+    /*
+     * Default File Storage Engine
+     * ===========================
+     *
+     * local|s3|db
+     */
     'storage'  => 'local',
     
+    /*
+     * Local Storage
+     * ================
+     */
     'local' => array(
         'path' => app_path() . '/storage/files/',
+    ),
+
+    /*
+     * File Uploading
+     * ================
+     */
+    'upload' => array(
+
+        /* * default name of `input:file */
+        'name' => 'file',
     ),
 
 );
