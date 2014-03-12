2b60aa6423bcb87467c37796830c052118e6297d
masamori <r.masamori@gmail.com>
2014-03-12 13:32:01 +0900

    Fix FormPartnerInterface

--- app/lib/Toiee/haik/Form/FormPartInterface.php
+++ app/lib/Toiee/haik/Form/FormPartInterface.php
@@ -3,6 +3,18 @@ namespace Toiee\haik\Form;
 
 interface FormPartInterface {
 
+    /**
+     * Return parts array
+     *
+     * return array parts detail
+     */
+    public function toArray();
+
+    /**
+     * Make part html 
+     *
+     * return string part html
+     */
     public function render();
 
 }
---
+++ app/lib/Toiee/haik/Form/Parts/Text.php
@@ -0,0 +1,9 @@
+<?php
+namespace Toiee\haik\Form\Parts;
+
+use Toiee\haik\Form\FormPartInterface;
+
+class Text implements FormPartInterface {
+    
+    
+}
